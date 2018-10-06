dfc2c19a5974a634c184bb91bb83084c7ecf211e
zefetorres <zefetorres@icloud.com>
2018-10-06 09:07:51 -0500

    add svae image in database

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     }
 
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
   componentWillMount() {
@@ -19,8 +21,14 @@ class App extends Component {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({
         user: user
-      })
-    })
+      });
+    });
+    //cada vez que se agregue un hijo a picture se renderice lo que estamos haciendo
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
+    });
   }
 
   handleAuth() {
@@ -37,6 +45,43 @@ class App extends Component {
     .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+  handleUpload(event) {
+    const file = event.target.files[0];
+    //creamos referencia
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`)
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({
+        uploadValue: percentage
+      })
+    }, error => {
+      console.log(error.message)
+    }, () => {      
+      task.snapshot.ref.getDownloadURL().then(downloadURL => {        
+        const record = {
+          photoURL: this.state.user.photoURL,
+          displayName: this.state.user.displayName,
+          image: downloadURL
+        };
+        console.log(record)
+        //creamos referencia a la DB
+        const dbRef = firebase.database().ref('pictures');
+        //insertamos registro a la BD
+        const newPicture = dbRef.push();
+        newPicture.set(record);
+      });
+
+      // task.snapshot.ref.getDownloadURL().then(downloadUrl => {
+      //   this.setState({
+      //     uploadValue: 100,
+      //     picture: downloadUrl
+      //   })
+      // })
+    }) //Estado de firebase al subir el archivo
+  }
+
   renderLoginButton() {
     //si el usuario esta logueado
     if(this.state.user) {
@@ -44,8 +89,21 @@ class App extends Component {
         <div>
           <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}</p>
-          <FileUpload />
           <button onClick={this.handleLogout}>Salir</button>
+          <FileUpload onUpload={this.handleUpload} />
+
+          {
+            this.state.pictures.map(picture => (
+              <div>
+                <img src={picture.image} />
+                <br/>
+                <img src={picture.photoURL} alt={picture.displayName} />
+                <br/>
+                <span>{picture.displayName}</span>
+              </div>
+            ))
+          }
+
         </div>
       );
     }else {      
